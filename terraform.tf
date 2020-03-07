9a0965eed95b2d78633da652dbf287c56e6da8f9
Nitin Bhalla <nitinbhalla@Nitins-MacBook-Pro.local>
2020-03-07 23:17:36 +1100

    fixed az index to 3

--- terraform.tf
+++ terraform.tf
@@ -144,7 +144,7 @@ resource "aws_db_instance" "db" {
 resource "aws_subnet" "app_subnet" {
   vpc_id     = aws_vpc.myvpc.id
   cidr_block = "10.0.11.0/24"
-  availability_zone = data.aws_availability_zones.available.names[3]
+  availability_zone = data.aws_availability_zones.available.names[2]
 
   tags = {
     app = "techtestapp"
